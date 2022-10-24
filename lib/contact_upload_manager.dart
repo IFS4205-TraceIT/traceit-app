ac707a246aa73cdfa9112bd7832f7296492fcc68
arraysius <16810847+arraysius@users.noreply.github.com>
2022-10-24 20:52:10 +0800

    Change debugPrint import to foundation.dart

--- lib/contact_upload_manager.dart
+++ lib/contact_upload_manager.dart
@@ -1,6 +1,6 @@
 import 'dart:convert';
 
-import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
 import 'package:traceit_app/const.dart';
 import 'package:traceit_app/server_auth.dart';
